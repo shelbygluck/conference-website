be35f623de54882a9e44db29938265d0705937f1
shelbygluck <55520179+shelbygluck@users.noreply.github.com>
2024-06-18 18:11:49 +0000

    introd_vuln

--- agenda/app.py
+++ agenda/app.py
@@ -15,20 +15,23 @@ def init():
     conn = sqlite3.connect('users.db')
     c = conn.cursor()
 
+    # Drop table if it exists
+    c.execute('DROP TABLE IF EXISTS users')
+
     # Create table
     c.execute('''
         CREATE TABLE users (
             id INTEGER PRIMARY KEY,
             name TEXT NOT NULL,
-            email TEXT NOT NULL UNIQUE
-            secret TEXT NOT NULL,
+            email TEXT NOT NULL UNIQUE,
+            password TEXT NOT NULL
         )
     ''')
 
     # Insert two users
-    c.execute("INSERT INTO users (name, email, password) VALUES (?, ?)", ('User 1', 'user1@example.com', 'user1secret'))
-    c.execute("INSERT INTO users (name, email, password) VALUES (?, ?)", ('User 2', 'user2@example.com', 'thisisverysecret'))
-
+    c.execute("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", ('User 1', 'user1@example.com', 'user1secret'))
+    c.execute("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", ('User 2', 'user2@example.com', 'thisisverysecret'))
+    
     # Save (commit) the changes and close the connection
     conn.commit()
     conn.close()
@@ -63,7 +66,8 @@ def main():
     @app.route('/query-by-name/<name>')
     def query_name(name: str):
         """Get info for an user by name."""
-        if auth.current_user() != name:
+        user = auth.current_user()
+        if user.name != name:
             return "Unauthorized", 403
 
         conn = sqlite3.connect('users.db')
@@ -81,6 +85,21 @@ def main():
         c = conn.cursor()
         c.execute('SELECT * FROM users WHERE email = ?', [email])
         user = c.fetchone()
+        conn.close()
+        return str(user)
+
+
+    @auth.login_required
+    @app.route('/query-by-id/<id>')
+    def query_id(id: str):
+        """Get info for an user by id."""
+        user = auth.current_user()
+        if user.id != id:
+            return "Unauthorized", 403
+        conn = sqlite3.connect('users.db')
+        c = conn.cursor()
+        c.execute('SELECT * FROM users WHERE id = ' + id)
+                user = c.fetchone()
         conn.close()
         return str(user)
 
